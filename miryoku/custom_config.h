df18dab95221b9e460f2fa9c68709accafaa1c93
Jack-Wong-dev <31869876+Jack-Wong-dev@users.noreply.github.com>
2025-02-16 18:53:01 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,10 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-// custom_config.h
-
-#define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
-&kp Q,         &kp W,         &kp E,         &kp R,         &kp T,         &kp Y,         &kp U,         &kp I,         &kp O,         &kp P,         \
-U_MT(LGUI, A),  U_MT(LALT, S),  U_MT(LCTRL, D),   U_MT(LSHFT, F),      &kp G,         &kp H,         U_MT(LSHFT, J),   U_MT(LCTRL, K),   U_MT(LALT, L),      &kp SEMI,    \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),      &kp C,         &kp V,         &kp B,         &kp N,         &kp M,         &kp COMMA,       U_MT(RALT, DOT),    U_LT(U_BUTTON, SLASH),\
-U_NP,         U_NP,         &kp ESC,      &kp BSPC,      &kp TAB,      &kp RET,      &kp SPACE,      &kp DEL,      U_NP,         U_NP
