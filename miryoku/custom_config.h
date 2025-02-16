ec9c238fe5716840afdd0851cd2e1245fde928f1
Jack-Wong-dev <31869876+Jack-Wong-dev@users.noreply.github.com>
2025-02-16 17:40:31 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q, &kp W, &kp E, &kp R, &kp T,      &kp Y, &kp U, &kp I, &kp O, &kp P, \
+&kp A, &kp S, &kp D, &kp F, &kp G,      &kp H, &kp J, &kp K, &kp L, &kp SEMI, \
+&kp Z, &kp X, &kp C, &kp V, &kp B,      &kp N, &kp M, &kp COMMA, &kp DOT, &kp SLASH, \
+U_NP, U_NP, &kp ESC, &kp BSPC, &kp TAB,  &kp RET, &kp SPACE, &kp DEL, U_NP, U_NP
